b57d437744d348a7397c241bf7c2e4d32887a8a3
Rennorb <18741506+SaculRennorb@users.noreply.github.com>
2023-01-08 14:00:45 +0100

    removed separate php8.1 implementation
    Its just not worth keeping a whole separate implementation just for so few minor changes. 8.0 should be compatible either way, we keep the tests for both on the same codebase
    
    closes #18

--- include/php/8.1/Database/LazyLoadMode.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Hardstuck\GuildWars2\BuildCodes\V2;
-
-enum LazyLoadMode : int {
-	case NONE = 0;
-	case OFFLINE_ONLY = 1;
-	case FULL = 2;
-}
--- include/php/8.1/Database/TraitIds.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Hardstuck\GuildWars2\BuildCodes\V2;
-/** @remarks Incomplete, mostly for fixing 'curiosities' in the API. */
-class TraitId {
-	use Util\Enum;
-
-	public const _UNDEFINED = 0;
-	public const Desperate_Decoy = 705;
-}
